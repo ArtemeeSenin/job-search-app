9d8e933c25eb0d366ad81260f79a073baec0d85b
ArtemeeSenin <artemee.senin@gmail.com>
2018-11-16 01:35:26 +0300

    removed filter shadow

--- src/index.js
+++ src/index.js
@@ -5,9 +5,21 @@ import { createStore } from 'redux'
 import reducer from './reducers'
 import middleware from './middleware'
 import { Provider } from 'react-redux'
+import * as firebase from 'firebase'
 import './index.scss'
 import App from './App';
 import initialState from './index.json';
+
+const config = {
+    apiKey: "AIzaSyAW9Yb7OPY8bB-fMi4-x2YYN7JEfeiraSQ",
+    authDomain: "job-search-app-f7fa1.firebaseapp.com",
+    databaseURL: "https://job-search-app-f7fa1.firebaseio.com",
+    projectId: "job-search-app-f7fa1",
+    storageBucket: "job-search-app-f7fa1.appspot.com",
+    messagingSenderId: "794082995107"
+}
+
+firebase.initializeApp(config);
 
 let store = createStore(reducer, initialState, middleware);
 
