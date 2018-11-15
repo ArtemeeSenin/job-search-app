d446311333dc34d5634243bbc1459ee63f38e5be
ArtemeeSenin <artemee.senin@gmail.com>
2018-11-15 09:22:41 +0300

    vacancy links in rating

--- src/components/containers/RatingTableVacancy.jsx
+++ src/components/containers/RatingTableVacancy.jsx
@@ -5,8 +5,8 @@ const RatingTableVacancy = ({ data }) => (
     <tr className="rating-table__table-row">
         <td className={`vacancy-summary vacancy-summary--${data.status.toLowerCase().split(' ').join('-')} rating-table__table-data`}>
             <div className="vacancy-summary__position">
-                <h2 className="vacancy-summary__position-name"><Link className="vacancy-summary__link" to="/vacancy/1">{data.position}</Link></h2>
-                <h3 className="vacancy-summary__position-company"><Link className="vacancy-summary__link" to="/company/1">{data.company}</Link></h3>
+                <h2 className="vacancy-summary__position-name"><Link className="vacancy-summary__link" to={`/vacancy/${data.id}`}>{data.position}</Link></h2>
+                <h3 className="vacancy-summary__position-company"><Link className="vacancy-summary__link" to={`/company/${data.id}`}>{data.company}</Link></h3>
             </div>
             <div className="vacancy-summary__conditions">
                 <div className="vacancy-summary__conditions-main-info-contrainer">
