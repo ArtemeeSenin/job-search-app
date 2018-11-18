db3040fc30396790e316a30443dfc53e68217fc1
ArtemeeSenin <artemee.senin@gmail.com>
2018-11-18 20:13:55 +0300

    removed commentaries for debug

--- src/actions/vacancies.js
+++ src/actions/vacancies.js
@@ -6,15 +6,6 @@ export const UPDATE_VACANCY = 'UPDATE_VACANCY';
 export const DELETE_VACANCY = 'DELETE_VACANCY'
 export const RECEIVE_VACANCIES = 'RECEIVE_VACANCIES';
 
-// export function addVacancy(data){
-//     data.id = generateUID();
-//     data.workDay = data.workDay * 60;
-
-//     return {
-//         type: ADD_VACANCY,
-//         data
-//     }
-// }
 function addVacancyAction(data, id){
     return {
         type: ADD_VACANCY,
@@ -43,13 +34,6 @@ export function addVacancy(data){
 
 }
 
-// export function updateVacancy(data){
-//     data.workDay = data.workDay * 60;
-//     return {
-//         type: UPDATE_VACANCY,
-//         data
-//     }
-// }
 function updateVacancyAction(data){
     return {
         type: UPDATE_VACANCY,
@@ -117,7 +101,6 @@ export function receiveVacancies(){
                 let data = [];
                 console.log(querySnapshot)
                 querySnapshot.forEach(function(doc) {
-                    // doc.data() is never undefined for query doc snapshots
                     console.log(doc, doc.data());
                     data.push({
                         ...doc.data(),
@@ -131,15 +114,3 @@ export function receiveVacancies(){
             });
     }
 }
-
-// .add({
-//             name: "Tokyo",
-//             country: "Japan"
-//         })
-//         .then(function(docRef) {
-//             console.log("Document written with ID: ", docRef.id);
-//             dispatch(getVacancies())
-//         })
-//         .catch(function(error) {
-//             console.error("Error adding document: ", error);
-//         });
