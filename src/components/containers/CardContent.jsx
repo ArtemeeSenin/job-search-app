968448eef3188e4693ce70a8846c901ec22c556b
ArtemeeSenin <artemee.senin@gmail.com>
2018-11-18 23:18:19 +0300

    toggle comment and description

--- src/components/containers/CardContent.jsx
+++ src/components/containers/CardContent.jsx
@@ -1,33 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import cx from 'classnames'
 import FeatureList from './FeatureList';
 
-const CardContent = ({data, features}) => {
-    return (
-        <div className={cx(
-            'card-info__content',
-            { 'card-info__content--column': !features }
-        )}>
-            <section className="card-info__comment">
-                <h3 className="heading heading--large">Personal commentary</h3>
-                <div className="card-info__comment-text">
-                    { data.commentary }
-                </div>
-            </section>
-            { features
-                ? <section className="card-info__conditions">
-                    <h3 className="heading heading--large card-info__conditions-heading">Conditions</h3>
-                    <FeatureList data={ features }/>
+class CardContent extends Component {
+    constructor(){
+        super();
+        this.state ={
+            descriptionToggled: false,
+            commentToggled: false
+        }
+    }
+    render(){
+        const {data, features} = this.props
+        return (
+            <div className={cx(
+                'card-info__content',
+                { 'card-info__content--column': !features }
+            )}>
+                <section
+                    className={cx(
+                        'card-info__comment',
+                        {'card-info__comment--is-active': this.state.commentToggled}
+                    )}
+                    onClick={() => this.setState({commentToggled: !this.state.commentToggled})}
+                >
+                    <h3 className="heading heading--large">Personal commentary</h3>
+                    <div className="card-info__comment-text">
+                        { data.commentary }
+                    </div>
                 </section>
-                : null }
-            <section className="card-info__description">
-                <h3 className="heading heading--large">Job description</h3>
-                <div className="card-info__description-text">
-                    { data.description }
-                </div>
-            </section>
-        </div>
-    )
+                { features
+                    ? <section className="card-info__conditions">
+                        <h3 className="heading heading--large card-info__conditions-heading">Conditions</h3>
+                        <FeatureList data={ features }/>
+                    </section>
+                    : null }
+                <section
+                    className={cx(
+                        'card-info__description',
+                        {'card-info__description--is-active': this.state.descriptionToggled}
+                    )}
+                    onClick={() => this.setState({descriptionToggled: !this.state.descriptionToggled})}
+                    >
+                    <h3 className="heading heading--large">Job description</h3>
+                    <div className="card-info__description-text">
+                        { data.description }
+                    </div>
+                </section>
+            </div>
+        )
+    }
 }
 
 export default CardContent;
--- src/components/containers/CompanyCardContent.jsx
+++ src/components/containers/CompanyCardContent.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import cx from 'classnames'
 
 function createMarkup(markup) { return { __html: markup }; };
 
-const CardContent = ({ data }) => {
-    const logo = data.companyLogo ? `<img src="${data.companyLogo}" class="card-info__logo" alt="${data.company}" />` : '';
-    const description = data.companyDescription ? data.companyDescription : '';
-    return (
-        <div className="card-info__content card-info__content--column">
-            { data.companyCommentary
-                ? <section className="card-info__comment">
-                <h3 className="heading heading--large">Personal commentary</h3>
-                <div className="card-info__comment-text">
-                    {data.companyCommentary}
-                </div>
-            </section> : null }
-            <section className="card-info__description">
-                <div className="card-info__description-text" dangerouslySetInnerHTML={createMarkup(logo + description)} />
-            </section>
-        </div>
-    )
+class CardContent extends Component {
+    constructor(){
+        super();
+        this.state ={
+            descriptionToggled: false,
+            commentToggled: false
+        }
+    }
+    render() {
+        const { data } = this.props
+        const logo = data.companyLogo ? `<img src="${data.companyLogo}" class="card-info__logo" alt="${data.company}" />` : '';
+        const description = data.companyDescription ? data.companyDescription : '';
+        return (
+            <div className="card-info__content card-info__content--column">
+                { data.companyCommentary
+                    ? <section
+                        className={cx(
+                            'card-info__comment',
+                            {'card-info__comment--is-active': this.state.commentToggled}
+                        )}
+                        onClick={() => this.setState({commentToggled: !this.state.commentToggled})}
+                    >
+                    <h3 className="heading heading--large">Personal commentary</h3>
+                    <div className="card-info__comment-text">
+                        {data.companyCommentary}
+                    </div>
+                </section> : null }
+                <section
+                    className={cx(
+                        'card-info__description',
+                        {'card-info__description--is-active': this.state.descriptionToggled}
+                    )}
+                    onClick={() => this.setState({descriptionToggled: !this.state.descriptionToggled})}
+                >
+                    <div className="card-info__description-text" dangerouslySetInnerHTML={createMarkup(logo + description)} />
+                </section>
+            </div>
+        )
+    }
 }
 
 
